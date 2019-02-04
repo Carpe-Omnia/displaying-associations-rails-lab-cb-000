c737e32b7c99e9bde171a322d707708fb4b074cc
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-04 18:35:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,6 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all 
+    @songs = Song.all
   end
 
   def show
@@ -49,4 +49,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title)
   end
 end
-
